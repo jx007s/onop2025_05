43f0c85bb159985a97ddef81bc1639ff82bd324a
DESKTOP-7CHP8SF\user <jx007s@nate.com>
2025-05-21 11:16:30 +0900

    변수

---
+++ src/basic/VarMain.java
@@ -0,0 +1,18 @@
+package basic;
+
+public class VarMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x;
+		
+		//5 + 3 = x;
+		
+		x = 5 + 3;  // 대입 연산자  :  좌항(변수) = 깂;
+		
+		System.out.println(x);
+
+	}
+
+}
