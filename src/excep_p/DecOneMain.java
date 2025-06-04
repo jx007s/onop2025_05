01ba1ebdfdfa51c0e2b647df45eccfe3e96ade8b
DESKTOP-7CHP8SF\user <jx007s@nate.com>
2025-06-04 17:20:34 +0900

    6_4_6

--- src/excep_p/DecOneMain.java
+++ src/excep_p/DecOneMain.java
@@ -4,11 +4,32 @@ import java.util.Scanner;
 
 
 class ReThrowsLogin{
+	final Scanner sc = new Scanner(System.in);
+	final String pid, ppw, pname;
+	
+	ReThrowsLogin(String pid, String ppw, String pname) {
+		
+		this.pid = pid;
+		this.ppw = ppw;
+		this.pname = pname;
+	}
 
-	//로그인 처리 만 있음 - try~catch 가 없다.
-	void login(){
- 
- 
+
+
+
+	void login() throws Exception{
+		System.out.print("id : ");
+		String uid = sc.nextLine();
+		if(!uid.equals(pid)) {
+			throw new Exception("ID가 존재하지 않습니다.");					
+		}
+
+		System.out.print("pw : ");
+		String upw = sc.nextLine();
+		if(!upw.equals(ppw)) {
+			throw new Exception("암호가 일치하지 않습니다.");
+		}
+		System.out.println(pname+"님 안녕하세요");
     }
 }
  
@@ -19,23 +40,12 @@ public class DecOneMain {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		String pid = "qwer", ppw = "1234";
+		ReThrowsLogin rt = new ReThrowsLogin("zxcv","5678","장남건");
 	
 		while(true) {
 			try {
 				
-				System.out.print("id : ");
-				String uid = sc.nextLine();
-				if(!uid.equals(pid)) {
-					throw new Exception("ID가 존재하지 않습니다.");					
-				}
-
-				System.out.print("pw : ");
-				String upw = sc.nextLine();
-				if(!upw.equals(ppw)) {
-					throw new Exception("암호가 일치하지 않습니다.");
-				}
+				rt.login();
 				
 				break;
 			} catch (Exception e) {
@@ -43,7 +53,7 @@ public class DecOneMain {
 			}
 		}
 		
-		System.out.println("장동건님 안녕하세요");
+		
 
 	}
 
