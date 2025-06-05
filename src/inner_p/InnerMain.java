1c185f95ed84924734e04417cda9ef4ad4f996f3
DESKTOP-7CHP8SF\user <jx007s@nate.com>
2025-06-05 09:54:33 +0900

    6_5_1

---
+++ src/inner_p/InnerMain.java
@@ -0,0 +1,85 @@
+package inner_p;
+
+class ClaOuter{
+	String a = "outer_a";
+	private String b = "outer_private_b";
+	String c = "outer_c";
+	
+	void meth_1() {
+		System.out.println("outer_meth_1() 실행:"+a+","+b+","+c);
+	}
+	
+	private void meth_2() {
+		System.out.println("outer_private_meth_2() 실행");
+	}
+	void meth_3() {
+		System.out.println("outer_meth_3() 실행:"+a+","+b+","+c);
+	}
+	
+	//1. 멤버 Inner 클래스 (멤버 중첩 클래스)
+	class InnMem{
+		String a = "InnMem_a";
+		String d = "InnMem_d";
+		
+		void meth_1() {
+			System.out.println("InnMem_meth_1() 시작:");
+			System.out.println(a+","+b+","+c+","+d);
+			System.out.println("InnMem_meth_1() 끝:");
+		}
+		
+		void meth_4() {
+			System.out.println("InnMem_meth_4() 시작:");
+			meth_2();
+			meth_3();
+			System.out.println(a+","+b+","+c+","+d);
+			System.out.println("InnMem_meth_1() 끝:");
+		}
+	}
+}
+
+public class InnerMain {
+
+	public static void main(String[] args) {
+		ClaOuter co1 = new ClaOuter();
+		ClaOuter co2 = new ClaOuter();
+		co2.a = "바깥양반_a";
+		co2.c = "바깥양반_c";
+		System.out.println("co1 : "+co1.a);
+		co1.meth_1();
+		System.out.println("co2 : "+co2.a);
+		co2.meth_1();
+		
+		//InnMem im1;  단독 선언 및 생성불가
+		//멤버 inner클래스 선언
+		// outer클래스명.inner클래스명 변수명;
+		ClaOuter.InnMem im11;
+		// outer클래스명.inner클래스명 생성불가
+		//im11 = new ClaOuter.InnMem();
+		
+		// 생성 : outer인스턴스변수.new inner클래스생성자()
+		im11 = co1.new InnMem();
+		ClaOuter.InnMem im12 = co1.new InnMem();
+		
+		ClaOuter.InnMem im21 = co2.new InnMem();
+		
+		System.out.println("im11 >>>");
+		//outer 멤버변수 접근불가 : im11.c
+		//System.out.println(im11.a+","+im11.c+","+im11.d);
+		System.out.println(im11.a+","+im11.d);
+		im11.meth_1();
+		//im11.meth_3();
+		im11.meth_4();
+		
+		System.out.println("im12 >>>");
+		System.out.println(im12.a+","+im12.d);
+		im12.meth_1();
+		im12.meth_4();
+		
+		
+		System.out.println("im21 >>>");
+		System.out.println(im21.a+","+im21.d);
+		im21.meth_1();
+		im21.meth_4();
+	}
+
+}
