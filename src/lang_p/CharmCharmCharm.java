76b51618de6dbbb86c8a99f266ffa6aff25c4e07
DESKTOP-7CHP8SF\user <jx007s@nate.com>
2025-06-09 17:54:49 +0900

    6_9_4

--- src/lang_p/CharmCharmCharm.java
+++ src/lang_p/CharmCharmCharm.java
@@ -1,9 +1,35 @@
 package lang_p;
 
+import java.util.Scanner;
+
 public class CharmCharmCharm {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("추억의 게임 참참참");
+		String [] arr = " ,왼쪽,가운데,오른쪽".split(",");
+		while(true) {
+			System.out.println("0:종료, 1:왼쪽, 2:가운데, 3:오른쪽");
+			System.out.print("입력 : ");
+			
+			int user = sc.nextInt();
+			if(user==0) {
+				break;
+			}
+			
+			int com = (int)(Math.random()*3)+1;
+			String res = "패";
+			if(user==com) {
+				res = "승";
+			}
+			System.out.println("게이머:"+arr[user]);
+			System.out.println("컴퓨터:"+arr[com]);
+			System.out.println(res);
+		}
+		
+		System.out.println("게임종료");
+		
+		//가위 바위 보 게임을 만드세요
 
 	}
 
---
+++ src/lang_p/GBBMain.java
@@ -0,0 +1,38 @@
+package lang_p;
+
+import java.util.Scanner;
+
+public class GBBMain {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String [] arr = "가위,바위,보".split(",");
+		String [][] res = {				//user
+					{"비김","패","승"},	//0:가위
+					{"승","비김","패"},	//1:바위
+					{"패","승","비김"}	//2:보
+			//com	0:가위, 1:바위, 2:보
+		};
+		while(true) {
+			System.out.println("0:종료, 1:가위, 2:바위, 3:보");
+			System.out.print("입력 : ");
+			
+			int user = sc.nextInt();
+			if(user==0) {
+				break;
+			}
+			user--;
+			
+			int com = (int)(Math.random()*3);
+
+			System.out.println("게이머:"+arr[user]);
+			System.out.println("컴퓨터:"+arr[com]);
+			System.out.println(res[user][com]);
+		}
+		
+		System.out.println("게임종료");
+
+	}
+
+}
