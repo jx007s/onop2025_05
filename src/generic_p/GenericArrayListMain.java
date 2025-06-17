56099faf5d0a3430e99bf1aa1ef2198b655af1f2
DESKTOP-7CHP8SF\user <jx007s@nate.com>
2025-06-17 12:17:02 +0900

    6_17_3

--- src/generic_p/GenericArrayListMain.java
+++ src/generic_p/GenericArrayListMain.java
@@ -37,8 +37,8 @@ public class GenericArrayListMain {
 		ArrayList<Phone> ppArr = new ArrayList();
 		ArrayList<HandPhone> hhArr = new ArrayList();
 		ArrayList<FootPhone> ffArr = new ArrayList();
-		ArrayList<? super HandPhone> shArr = new ArrayList();
-		ArrayList<? extends HandPhone> ehArr = new ArrayList();
+		ArrayList<? super HandPhone> shArr = new ArrayList();  //ArrayList<HandPhone> 와 같음
+		ArrayList<? extends HandPhone> ehArr = new ArrayList();  // 모두 불가 - 의미 없음
 		ArrayList<TV> tvArr = new ArrayList();
 		
 		objArr.add(new TV());
@@ -60,5 +60,82 @@ public class GenericArrayListMain {
 		hhArr.add(new HandPhone());	//자신만 가능 (부모 불가)
 		//hhArr.add(new FootPhone());
 		System.out.println("hhArr "+hhArr);
+		
+//		ffArr.add(new TV());
+//		ffArr.add(new Phone());
+//		ffArr.add(new HandPhone());
+		ffArr.add(new FootPhone());//자신만 가능 (부모 불가)
+		System.out.println("ffArr "+ffArr);
+		
+//		shArr.add(new TV());
+//		shArr.add(new Phone());
+		shArr.add(new HandPhone());//자신만 가능 (부모 불가)
+//		shArr.add(new FootPhone());
+		
+		System.out.println("shArr "+shArr);
+		
+		objArr.add(new TV());
+		objArr.add(new Phone());
+		objArr.add(new HandPhone());
+		objArr.add(new FootPhone());
+		
+		System.out.println("shArr "+shArr);
+		
+//		ehArr.add(new TV());
+//		ehArr.add(new Phone());
+//		ehArr.add(new HandPhone());
+//		ehArr.add(new FootPhone());  모두 불가 - 의미 없음
+		
+		
+		tvArr.add(new TV());
+//		tvArr.add(new Phone());
+//		tvArr.add(new HandPhone());
+//		tvArr.add(new FootPhone());
+		
+		System.out.println("tvArr "+tvArr);
+		
+		//// 메소드 호출
+		methObj(objArr);
+		methObj(ppArr);
+		methObj(hhArr);
+		methObj(ffArr);
+		methObj(shArr);
+		methObj(ehArr);
+		methObj(tvArr);
+		
+//		methPP(objArr);		// Generic 이 설정된 메소드에서는 Object ArrayList는 하지 말것
+		methPP(ppArr);		//자신 + Object 호출 가능한 것으로 보이나 내부 코드에서 Object ArrayList 에러발생
+//		methPP(hhArr);		// 자식 불가
+//		methPP(ffArr);
+//		methPP(shArr);
+//		methPP(ehArr);
+//		methPP(tvArr);
+		
+		//methHH(objArr);
+		//methHH(ppArr);
+		methHH(hhArr);	// 자신만 가능 , 부모 불가
+//		methHH(ffArr);
+//		methHH(shArr);
+//		methHH(ehArr);
+//		methHH(tvArr);
+		
+		
+//		methHHEx(objArr);
+//		methHHEx(ppArr);
+		methHHEx(hhArr);
+//		methHHEx(ffArr);
+//		methHHEx(shArr);
+		methHHEx(ehArr);
+//		methHHEx(tvArr);
+		
 	}	
+	
+	static void methObj(ArrayList arr) { for (Object obj : arr) {	}}
+	static void methPP(ArrayList<Phone> arr) { for (Phone obj : arr) {	}}
+	static void methHH(ArrayList<HandPhone> arr) { for (HandPhone obj : arr) {	}}
+	static void methHHEx(ArrayList<? extends HandPhone> arr) { for (HandPhone obj : arr) {	}}
+	static void methHHSuper(ArrayList<? super HandPhone> arr) { 
+		//for (HandPhone obj : arr) {	}  에러발생
+		for (Object obj : arr) {	}  
+	}
 }
