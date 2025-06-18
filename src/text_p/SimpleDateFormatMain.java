10d83949719757fdcca258812cb9373354bdfc30
DESKTOP-7CHP8SF\user <jx007s@nate.com>
2025-06-18 22:19:16 +0900

    6_18_2

--- src/text_p/SimpleDateFormatMain.java
+++ src/text_p/SimpleDateFormatMain.java
@@ -1,12 +1,13 @@
 package text_p;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 public class SimpleDateFormatMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException  {
 		Date now = new Date();
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
@@ -20,7 +21,10 @@ public class SimpleDateFormatMain {
 				"E","EE","EEE","EEEE",
 				"F","z","Z",
 				
-				"a"
+				"a","H","HH","h","hh",
+				"m","mm",
+				"s","ss",
+				"S","SSS"
 				
 		};
 		
@@ -31,7 +35,37 @@ public class SimpleDateFormatMain {
 			sdf = new SimpleDateFormat(pp, loc);
 			System.out.println(pp+" : "+sdf.format(now));
 		}
-
+		
+		sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date ddd = sdf.parse("1997-03-20");
+		System.out.println(ddd);
+		
+		sdf = new SimpleDateFormat("yyMMdd");
+		ddd = sdf.parse("970320");
+		
+		System.out.println(ddd);
+		
+		sdf = new SimpleDateFormat("HH:mm:ss");
+		ddd = sdf.parse("13:45:27");
+		System.out.println(ddd);
+		
+		sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		ddd = sdf.parse("1999-06-23 13:45:27");
+		System.out.println(ddd);
+		
+		ddd = sdf.parse("1999-89-61 123:9888:1234");
+		System.out.println(ddd);
+		
+		now = sdf.parse("1989-11-28 05:07:30");
+		
+		loc = new Locale("en");
+		sdf = new SimpleDateFormat("''yy年 MMM dd일 (", loc);
+		String ttt = sdf.format(now)+
+				new SimpleDateFormat("E) HH:mm:ss", new Locale("ko")).format(now);
+		System.out.println(ttt);
+		
+		 /// '89年  nov 28일 (화) 05:07:30  형태로 출력해 주세요
+		 ///주민번호 -> 생년월일, 한국나이, 만나이, 올해생일, 다가올생일, 생일파티, DDay
 	}
 
 }
