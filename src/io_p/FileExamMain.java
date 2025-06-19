c6057325be2aa8455996b679e2869d73c76c79f3
user <user@TEACHER-PC>
2025-06-19 12:09:43 +0900

    6_19_3

--- src/io_p/FileExamMain.java
+++ src/io_p/FileExamMain.java
@@ -1,6 +1,7 @@
 package io_p;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 class FileStud{
@@ -14,9 +15,9 @@ class FileStud{
 		jum = new ArrayList<Integer>();
 		
 		for (int i = 1; i < arr.length; i++) {
-			jum.add(Integer.parseInt(arr[i]));
+			jum.add(Integer.parseInt(arr[i].trim()));
 		}
-		
+		calc();
 	}
 	private void calc() {
 		tot = 0;
@@ -45,6 +46,11 @@ public class FileExamMain {
 
 	public static void main(String[] args) {
 		try {
+			
+			
+			// 1. 입력부
+			ArrayList<FileStud> studs = new ArrayList<FileStud>();
+			
 			FileReader fr = new FileReader("fff/exam.txt");
 			
 			String line = "";
@@ -55,7 +61,9 @@ public class FileExamMain {
 				
 				//System.out.println(ch);
 				if(ch=='\n') {
-					System.out.println(line);
+					studs.add(new FileStud(line)) ;
+					//System.out.println(line);
+					
 					line="";
 				}else {
 					line+=ch;	
@@ -63,6 +71,21 @@ public class FileExamMain {
 			}
 			
 			fr.close();
+			
+			//2. 데이터 확인
+//			for (FileStud st : studs) {
+//				System.out.println(st);
+//			}
+			
+			// 3. 출력부
+			
+			FileWriter fw = new FileWriter("zzz/examRes.txt");
+			
+			for (FileStud st : studs) {
+				fw.write(st.toString()+"\n");
+			}
+			
+			fw.close();
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
@@ -70,3 +93,24 @@ public class FileExamMain {
 	}
 
 }
+/*
+원본파일 : fff/shape.txt
+5,6
+5
+5,6,8
+10
+10,20
+13,17,22
+
+ 
+결과파일 : zzz/shapeRes.txt
+
+직사각형 	넓이, 둘레
+원 		넓이, 둘레
+직각삼각형	넓이, 둘레
+직사각형 	넓이, 둘레
+원 		넓이, 둘레
+직각삼각형	넓이, 둘레  
+
+ * */
+
